0eee887c5d53881e619f41ff74cdb7de4173c405
GING01 <andres.paredes@epn.edu.ec>
2017-05-17 14:47:53 -0500

    clase17/04/2017

--- Angular/a-web/src/app/app.component.ts
+++ Angular/a-web/src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'twj-raiz',
+  templateUrl: "./app.component.html" ,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  usuario:UsuarioInt = {
+    "nombre":"Andres",
+    "apellido":"Paredes"
+  }
+
+  constructor(){
+    console.log("Constructor");
+    this.holaMundo();
+    console.log(
+      this.saludar(this.usuario.nombre,
+        this.usuario.apellido
+      ));
+  }
+
+  holaMundo(){
+    console.log("Hola mundo");
+  }
+  saludar(nombre:string,
+          apellido?:string):string{
+    return `Hola ${nombre} ${apellido}`;
+  }
+
+}
+
+
+
+
+interface UsuarioInt{
+  nombre:string,
+  apellido:string
 }
