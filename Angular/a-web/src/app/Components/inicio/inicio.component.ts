675e7e7b4511e78ebf3ffe83fce27d4448c4039b
GING01 <andres.paredes@epn.edu.ec>
2017-06-05 09:52:19 -0500

    deber

--- Angular/a-web/src/app/Components/inicio/inicio.component.ts
+++ Angular/a-web/src/app/Components/inicio/inicio.component.ts
@@ -74,3 +74,19 @@ export class InicioComponent implements OnInit {
   }
 
 }
+interface PlanetaStarWars {
+  name: string;
+  rotation_period: number;
+  orbital_period: number;
+  diameter: number;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: number;
+  population: number;
+  residents: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
